2d936ead73a3cb7bf932606111715c18b78c5e93
Nathan Sprague <Nathan Sprague>
2022-06-06 12:24:52 -0400

    add sphinx build stuff

---
+++ doc/conf.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+html_static_path = []
+
+# enable autodoc to load local modules
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "JMU Python Gradescope Utils"
+copyright = "2020"
+author = "JMU People"
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx",'sphinx.ext.napoleon' ]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+templates_path = ["_templates"]
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None)
+}
+html_theme_options = {"nosidebar": True}
