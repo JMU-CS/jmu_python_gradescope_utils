079e1236a941660da17df4b016cbb666f62166b7
Nathan Sprague <nathan.r.sprague@gmail.com>
2023-01-16 15:21:47 -0500

    remove branch target from setup.sh

--- jmu_gradescope_utils/data/setup.sh
+++ jmu_gradescope_utils/data/setup.sh
@@ -4,4 +4,4 @@ apt-get install -y python3 python3-pip python3-dev
 
 pip3 install -r /autograder/source/requirements.txt
 
-pip3 install git+https://github.com/JMU-CS/jmu_python_gradescope_utils.git@gui-and-tidy
+pip3 install git+https://github.com/JMU-CS/jmu_python_gradescope_utils.git
